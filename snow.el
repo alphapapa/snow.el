ab8bde496c7ee40827648aeff290a289dbbed240
Adam Porter <adam@alphapapa.net>
2018-12-24 05:51:43 -0600

    Fancier

--- snow.el
+++ snow.el
@@ -2,8 +2,8 @@
 
 (defvar snowflakes-flakes nil)
 
-(defvar snowflakes-amount 1)
-(defvar snowflakes-rate 0.1)
+(defvar snowflakes-amount 2)
+(defvar snowflakes-rate 0.15)
 (defvar snowflakes-timer nil)
 
 (defface snowflakes-face
@@ -15,7 +15,14 @@
 (defvar-local snowflakes-string nil)
 
 (cl-defstruct snowflake
-  x y)
+  x y mass char)
+
+(defun snowflake-get-flake (z)
+  (propertize (pcase z
+                ((pred (< 90)) "❄")
+                ((pred (< 50)) "*")
+                (_ "."))
+              'face 'snowflakes-face))
 
 (defun snowflakes--update-buffer (buffer)
   (with-current-buffer buffer
@@ -23,9 +30,10 @@
           (cols (window-width)))
       (setq snowflakes-flakes
             (append snowflakes-flakes
-                    (cl-loop for i from 0 to snowflakes-amount
+                    (cl-loop for i from 0 to (random snowflakes-amount)
                              for x = (random cols)
-                             for flake = (make-snowflake :x x :y 0)
+                             for mass = (random 100)
+                             for flake = (make-snowflake :x x :y 0 :mass mass :char (snowflake-get-flake mass))
                              do (snowflakes-draw flake)
                              collect flake)))
       (setq snowflakes-flakes
@@ -34,24 +42,21 @@
                                        ;; Delete old flake
                                        (snowflakes-draw flake 'delete)
                                        ;; Move flake
-                                       (when (> (random 10) 8)
-                                         (cl-incf (snowflake-x flake) (cl-case (random 2)
+                                       (when (> (random 100) (snowflake-mass flake))
+                                         (cl-incf (snowflake-x flake) (pcase (random 2)
                                                                         (0 -1)
                                                                         (1 1)))
                                          (setf (snowflake-x flake) (clamp 0 (snowflake-x flake) (1- cols))))
-                                       (cl-incf (snowflake-y flake)
-                                                (pcase (random 10)
-                                                  ((pred (> 2)) 0)
-                                                  ((pred (< 7)) 2)
-                                                  (_ 1))
-                                                )
+                                       (when (> (random 100) (/ (snowflake-mass flake) 2))
+                                         (cl-incf (snowflake-y flake)))
                                        (unless (>= (snowflake-y flake) (1- lines))
                                          ;; Redraw flake
                                          (snowflakes-draw flake)
                                          ;; Return moved flake
                                          flake))
                      when new-flake
-                     collect new-flake)))))
+                     collect new-flake)))
+    (setq mode-line-format (format "%s flakes" (length snowflakes-flakes)))))
 
 (defun snowflakes-draw (flake &optional delete)
   (save-excursion
@@ -61,7 +66,7 @@
     (delete-char 1)
     (insert (if delete
                 " "
-              snowflakes-string))))
+              (snowflake-char flake)))))
 
 (defun let-it-snow ()
   (interactive)
@@ -79,7 +84,31 @@
           (insert (make-string (window-text-width) ? )
                   "\n")))
       (goto-char (point-min))
-      (setq snowflakes nil)
+      (setq snowflakes-flakes nil)
       (setq snowflakes-timer
             (run-at-time nil 0.1 (apply-partially #'snowflakes--update-buffer (current-buffer))))
       (pop-to-buffer (current-buffer)))))
+
+(defun snowflakes-overlay ()
+  (let* ((s".      *    *           *.       *   .                      *     .
+               .   .                   __   *    .     * .     *
+    *       *         *   .     .    _|__|_        *    __   .       *
+  .  *  /\       /\          *        ('')    *       _|__|_     .
+       /  \   * /  \  *          .  <( . )> *  .       ('')   *   *
+  *    /  \     /  \   .   *       _(__.__)_  _   ,--<(  . )>  .    .
+      /    \   /    \          *   |       |  )),`   (   .  )     *
+   *   `||` ..  `||`   . *.   ... ==========='`   ... '--`-` ... * .")
+         (width (cl-loop for line in (s-lines s)
+                         maximizing (length line)))
+         (height (length (s-lines s)))
+         (x (- (window-text-width) width))
+         (y (- (window-text-height) height)))
+    (save-excursion
+      (goto-char (point-min))
+      (forward-line (1- y))
+      (cl-loop for line in (s-lines s)
+               do (progn
+                    (forward-line 1)
+                    ;; FIXME x
+                    ;;  (forward-char x)
+                    (ov (point) (point) 'before-string line))))))
